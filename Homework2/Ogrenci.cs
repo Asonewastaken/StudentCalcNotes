fe81892ab046139854b35d68352eaf725430b243
ashaf <ashaf@ASONE>
2024-01-28 15:38:29 +0300

    Add project files.

---
+++ Homework2/Ogrenci.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    internal class Ogrenci
+    {
+        private string _ogrenciNo { get; set; }
+
+        public double vize { get; set; }
+        public double final { get; set; }
+
+        public Ogrenci(string ogrenciNo)
+        {
+            if (string.IsNullOrEmpty(ogrenciNo))
+            {
+                throw new Exception("Ogrenic NO girilmelidir !");
+            }
+
+            if (ogrenciNo.Length < 9)
+            {
+                throw new Exception("Ogrenic NO 9 haneli olmalıdır !");
+            }
+
+            _ogrenciNo = ogrenciNo;
+        }
+
+        public string OgrenciNumarasi()
+        {
+            return _ogrenciNo;
+        }
+
+        public double OrtalamaBul()
+        {
+            double ortalama = vize * 0.4 + final * 0.7;
+
+            return ortalama;
+        }
+    }
+}
---
+++ Homework2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen Not Ortalamasını Öğrenmek İstediğiniz Öğrencinin Numarasını Giriniz: ");
+            string studentNumber = Console.ReadLine();
+
+            Ogrenci ogr1 = new Ogrenci(studentNumber);
+
+            Console.WriteLine("Vize notunu giriniz:");
+
+
+            ogr1.vize = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Final notunu giriniz:");
+            ogr1.final = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(string.Format("{0} numaralı öğrencinin ortalaması: {1}", ogr1.OgrenciNumarasi(), ogr1.OrtalamaBul()));
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
